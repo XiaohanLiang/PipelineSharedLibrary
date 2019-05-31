314e3a15a5ac0de810e5b6881d5e213ec2680018
liangxiaohan <liangxiaohan95@gmail.com>
2019-05-31 11:44:28 +0800

    First try

---
+++ vars/initEnv.groovy
@@ -0,0 +1,26 @@
+#!/usr/bin/env groovy
+
+def initEnv(String credential) {
+
+  echo "Entered Shared Library"
+
+  set +x
+
+  executeCommand("git init ${$WORKSPACE}/workspace")
+  executeCommand(":cd $WORKSPACE/workspace")
+
+//  git config --local --unset credential.helper
+//
+//  mkdir -p $WORKSPACE/.JD_CODE_BUILD
+//
+//  git config credential.helper "store --file=$WORKSPACE/.JD_CODE_BUILD/.git-credentials"
+//
+//  env|grep 'COMPILER_\|GIT_\|WORKSPACE' > .JD_CODE_BUILD/buildRuntimeEnv
+//
+//  cat /var/tmp/REGION_ID >> .JD_CODE_BUILD/buildRuntimeEnv
+
+}
+
+def executeCommand(String command) {
+  ['/bin/bash', '-c', ${command}].execute()
+}
