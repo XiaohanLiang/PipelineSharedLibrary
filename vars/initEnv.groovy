0e64866f37a8356ae1dd910d4e6c1a8598994a9b
liangxiaohan <liangxiaohan95@gmail.com>
2019-05-31 17:39:23 +0800

    Modifying initEnv

--- vars/initEnv.groovy
+++ vars/initEnv.groovy
@@ -1,27 +1,51 @@
 #!/usr/bin/env groovy
+import java.io.File
 
-def call(String credential) {
+def call() {
 
-  echo "Entered Shared Library"
+    // mkdir + meta(Store some runtime meta info)
+    //         ws(We use to clone,build,upload,etc)
+    String generalWorkingDirectory = System.getenv("WORKSPACE")
+    assert length(generalWorkingDirectory) > 0 : "Cannot find WORKSPACE"
+    String meta = generalWorkingDirectory + "/.JD_CODE_BUILD"
+    makeDir(meta)
+    String ws = generalWorkingDirectory + "/workspace"
+    makeDir(ws)
+    String runtimeEnvFile = meta + "/buildRuntimeEnv"
+    touchFile(runtimeEnvFile)
 
-  executeCommand("set +x")
+    // Read task runtime variable
+    File f = new File(runtimeEnvFile)
+    Map envMap =  System.getenv()
+    envMap.each {
+        String name = it.key
+        if (name.matches("COMPILER_(.*)") || name.matches("GIT_(.*)") || name == "WORKSPACE") {
+            f << it.key << "=" << it.value << "\n"
+        }
+    }
+    f << readFile("/var/tmp/REGION_ID") << "\n"
 
-  executeCommand("git init ${$WORKSPACE}/workspace")
-
-  executeCommand("cd $WORKSPACE/workspace")
-
-//  git config --local --unset credential.helper
-//
-//  mkdir -p $WORKSPACE/.JD_CODE_BUILD
-//
-//  git config credential.helper "store --file=$WORKSPACE/.JD_CODE_BUILD/.git-credentials"
-//
-//  env|grep 'COMPILER_\|GIT_\|WORKSPACE' > .JD_CODE_BUILD/buildRuntimeEnv
-//
-//  cat /var/tmp/REGION_ID >> .JD_CODE_BUILD/buildRuntimeEnv
+    // Git Operations
+    String setConfigCommand = "git config --local --unset credential.helper"
+    String setCredentialCmd = "git config credential.helper \"store --file="+ generalWorkingDirectory +"/.JD_CODE_BUILD/.git-credentials\""
+    def setConfig = setConfigCommand.execute()
+    def setCredential = setCredentialCmd.execute()
 
 }
 
-def executeCommand(String command) {
-  ['/bin/bash', '-c', ${command}].execute()
+
+
+void makeDir(String path){
+    File f = new File(path)
+    f.mkdir()
+}
+
+void touchFile(String filePath){
+    def newFile = new File(filePath)
+    newFile.createNewFile()
+}
+
+String readFile(String filePath) {
+    File file = new File(filePath)
+    return file.text
 }
