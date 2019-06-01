efa366958d8dda290d7f308dd9143ef50179baf2
liangxiaohan <liangxiaohan95@gmail.com>
2019-06-01 11:24:41 +0800

    Fix a bug

--- vars/deleteWorkspace.groovy
+++ vars/deleteWorkspace.groovy
@@ -1,6 +1,14 @@
 #!/usr/bin/env groovy
 import java.io.File
 
+/**
+ * Todo -
+ *         Users may want to keep their workspace, but deleting their
+ *         runtime env is a must, so we have to provide two options:
+ *                  Remove Ws -> Delete entire path
+ *                  Keep Ws -> Delete .JD_CODE_BUILD only
+ *
+ */
 def call(def path){
     assert path.length()>0 :"Empty path given"
     def ws = new File(path)
