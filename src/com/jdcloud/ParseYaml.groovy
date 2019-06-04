81c1546ff335c3a692bfd8e1cd3c86562b9e1a49
liangxiaohan <liangxiaohan95@gmail.com>
2019-06-04 11:54:19 +0800

    Script commit

--- src/com/jdcloud/ParseYaml.groovy
+++ src/com/jdcloud/ParseYaml.groovy
@@ -1,19 +1,8 @@
 package com.jdcloud
-import com.jdcloud.Address
-import com.jdcloud.Cmds
-import org.apache.ivy.ant.AddPathTask
-import org.junit.Assert
+
 @Grab(group='org.yaml', module='snakeyaml', version='1.17')
 import org.yaml.snakeyaml.Yaml
-import org.yaml.snakeyaml.constructor.Constructor
 
-import javax.xml.ws.soap.Addressing
-
-/*
-  Discard in future
- *
-
-*/
 class ParseYaml{
 
     def Cmds c
@@ -39,7 +28,7 @@ class ParseYaml{
 //        FileInputStream stream = new FileInputStream(file)
 //        def c = yaml.loadAs(stream, Address.class)
         Address cc = new Address("123","123","123","123")
-        def c = yaml.load(("/root/bb.yaml" as File).text)
+        def c = yaml.load(("/root/build.yaml" as File).text)
         return c
     }
 }
