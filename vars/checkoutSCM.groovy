ad2aa7a38de133a52f2d91cd4d6473120a71d7e6
liangxiaohan <liangxiaohan95@gmail.com>
2019-06-10 17:30:18 +0800

    Script commit

--- vars/checkoutSCM.groovy
+++ vars/checkoutSCM.groovy
@@ -5,11 +5,14 @@ def call(def env){
 
     env.Branch = 'master'
     env.Url = "https://github.com/XiaohanLiang/hello"
+    env.UserWorkSpace = env.WORKSPACE + "/workspace/"
 
-    checkout changelog: false, poll: false,
-            scm: [ $class: 'GitSCM', branches: [[name: env.Branch]],
-                   doGenerateSubmoduleConfigurations: false,
-                   extensions: [],
-                   submoduleCfg: [],
-                   userRemoteConfigs: [[url: env.Url]]]
+    dir(env.UserWorkSpace){
+        checkout changelog: false, poll: false,
+                scm: [ $class: 'GitSCM', branches: [[name: env.Branch]],
+                       doGenerateSubmoduleConfigurations: false,
+                       extensions: [],
+                       submoduleCfg: [],
+                       userRemoteConfigs: [[url: env.Url]]]
+    }
 }
