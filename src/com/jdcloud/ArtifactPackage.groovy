1217c70788fa6b093d50ce5163b721d379a926ea
liangxiaohan <liangxiaohan95@gmail.com>
2019-06-10 16:01:07 +0800

    Script commit

--- src/com/jdcloud/ArtifactPackage.groovy
+++ src/com/jdcloud/ArtifactPackage.groovy
@@ -117,6 +117,8 @@ class ArtifactPackage {
     def RecordRuntimeEnv(String pattern) {
         // Assume buildRuntimeEnv exists
         PrintWriter pencil = new PrintWriter(this.MetaSpace + "buildRuntimeEnv")
+        this.script.echo "Target -> " + this.MetaSpace + "buildRuntimeEnv"
+        this.script.echo "Pattern -> " + pattern
         pencil.println(pattern)
     }
 
