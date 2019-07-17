bc7a205afce8fed30f78f9d2c577eb61a437b28d
xiaohan.liang <Liangxiaohan95@gmail.com>
2019-07-17 20:29:49 +0800

    Script commit

--- src/com/jdcloud/FromYaml.groovy
+++ src/com/jdcloud/FromYaml.groovy
@@ -2,7 +2,7 @@
 package com.jdcloud
 
 @Grab(group='org.yaml', module='snakeyaml', version='1.17')
-//import org.yaml.snakeyaml.Yaml
+import org.yaml.snakeyaml.Yaml
 import static org.junit.Assert.*
 import java.lang.ProcessBuilder
 
