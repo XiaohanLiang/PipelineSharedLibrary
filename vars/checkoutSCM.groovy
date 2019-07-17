3fe95c894e5ba0a8a63c96b73b014ee9310324b1
xiaohan.liang <Liangxiaohan95@gmail.com>
2019-07-17 17:22:28 +0800

    Script commit

--- vars/checkoutSCM.groovy
+++ vars/checkoutSCM.groovy
@@ -5,11 +5,23 @@ def call(def env){
 
     dir(env.UserWorkSpace){
 
+        sh """
+            git init ${env.UserWorkSpace}
+            
+        """
+
+        sh(returnStdout: true,script:"git config --local --unset credential.helper")
+        
+        sh """
+            git config credential.helper store --file=${env.MetaSpace}.git-credentials
+            echo ${env.SCM_CREDENTIAL} > ${env.MetaSpace}.git-credentials
+        """
+
         checkout changelog: false, poll: false,
                 scm: [ $class: 'GitSCM', branches: [[name: env.SCM_BRANCH]],
                        doGenerateSubmoduleConfigurations: false,
                        extensions: [],
                        submoduleCfg: [],
-                       userRemoteConfigs: [[authToken:env.SCM_CREDENTIAL, url: env.SCM_URL]]]
+                       userRemoteConfigs: [[url: env.SCM_URL]]]
     }
 }
