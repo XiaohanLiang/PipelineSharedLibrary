c98a5108f9eda7c0193e27b2d10d19176257a631
xiaohan.liang <Liangxiaohan95@gmail.com>
2019-07-17 17:35:16 +0800

    Script commit

--- vars/checkoutSCM.groovy
+++ vars/checkoutSCM.groovy
@@ -5,23 +5,25 @@ def call(def env){
 
     dir(env.UserWorkSpace){
 
-        sh """
-            git init ${env.UserWorkSpace}
-            
-        """
+//        sh """
+//            git init ${env.UserWorkSpace}
+//
+//        """
 
 //        sh(returnStdout: true,script:"git config --local --unset credential.helper")
-        
-        sh """
-            git config credential.helper store --file=${env.MetaSpace}.git-credentials
-            echo ${env.SCM_CREDENTIAL} > ${env.MetaSpace}.git-credentials
-        """
 
-        checkout changelog: false, poll: false,
-                scm: [ $class: 'GitSCM', branches: [[name: env.SCM_BRANCH]],
-                       doGenerateSubmoduleConfigurations: false,
-                       extensions: [],
-                       submoduleCfg: [],
-                       userRemoteConfigs: [[url: env.SCM_URL]]]
+//        sh """
+//            git config credential.helper store --file=${env.MetaSpace}.git-credentials
+//            echo ${env.SCM_CREDENTIAL} > ${env.MetaSpace}.git-credentials
+//        """
+//
+//        checkout changelog: false, poll: false,
+//                scm: [ $class: 'GitSCM', branches: [[name: env.SCM_BRANCH]],
+//                       doGenerateSubmoduleConfigurations: false,
+//                       extensions: [],
+//                       submoduleCfg: [],
+//                       userRemoteConfigs: [[url: env.SCM_URL]]]
+        git branch: ${env.SCM_BRANCH}, url: ${env.SCM_URL}
+
     }
 }
