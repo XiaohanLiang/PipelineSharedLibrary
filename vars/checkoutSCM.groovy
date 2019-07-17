d84151a8fddaaff1de439ea3d6fb67a9797cd71d
xiaohan.liang <Liangxiaohan95@gmail.com>
2019-07-17 19:38:03 +0800

    Script commit

--- vars/checkoutSCM.groovy
+++ vars/checkoutSCM.groovy
@@ -16,7 +16,7 @@ def call(def env){
         echo ${env.SCM_CREDENTIAL} > ${env.MetaSpace}.git-credentials
         """
 
-        sh(returnStdout: true,script:"${env.MetaSpace}scm.sh")
+        sh("bash ${env.MetaSpace}scm.sh")
 
         checkout changelog: false, poll: false,
                 scm: [ $class: 'GitSCM', branches: [[name: "${env.SCM_BRANCH}"]],
