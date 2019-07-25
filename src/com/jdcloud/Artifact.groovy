d894a4b39189974e67ca321755c751f001843991
xiaohan.liang <Liangxiaohan95@gmail.com>
2019-07-25 15:36:12 +0800

    Script commit

--- src/com/jdcloud/Artifact.groovy
+++ src/com/jdcloud/Artifact.groovy
@@ -41,6 +41,13 @@ class Artifact {
             s.echo "Use image tag - ${env.DOCKER_IMAGE_TAG}"
             this.BuildTag = env.DOCKER_IMAGE_TAG
         }else{
+
+            if (env.hasProperty('DOCKER_IMAGE_TAG')){
+                s.echo "Has image tag -> ${env.DOCKER_IMAGE_TAG}"
+            }else{
+                s.echo "Donnot has docker image tag"
+            }
+
             s.echo "Use random image tag"
             this.BuildTag = env.BUILD_TAG
         }
